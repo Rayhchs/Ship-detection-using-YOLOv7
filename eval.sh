a5b491f05e649e9d8f549e448a1e7aaff0e40e40
Rayhchs <ray1515123@gmail.com>
2022-08-03 19:49:55 +0800

    fix bug

--- eval.sh
+++ eval.sh
@@ -9,7 +9,7 @@ STRING="[*] Evaluating"
 echo $STRING
 
 # For Single GPU (p5 model)
-python evaluation.py --pred_txt ./results/ --label_txt ./labels/test/ --img_size 256
+python evaluation.py --pred_txt ./results/ --label_txt ./labels/test/
 
 # For Multi-GPU (p5 model)
 # python -m torch.distributed.launch --nproc_per_node 4 --master_port 9527 train.py --workers 8 --device 0,1,2,3 --sync-bn --batch-size 128 --data sar/sar.yaml --img 640 640 --cfg cfg/training/yolov7.yaml --weights '' --name yolov7 --hyp data/hyp.scratch.p5.yaml
